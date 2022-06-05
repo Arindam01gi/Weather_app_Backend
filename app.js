78e2a7be49dcee0000acb91b143e3b90c1b7f918
Arindam Maiti <76761507+Arindam01gi@users.noreply.github.com>
2022-06-05 07:34:19 +0530

    Third commit

--- app.js
+++ app.js
@@ -37,6 +37,6 @@ const url ="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+
 })
 
 
-app.listen(3000,function(){
+app.listen(process.env.PORT||3000,function(){
   console.log("Server is running at port 3000");
 });
